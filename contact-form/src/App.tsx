a2b51f8e292bc008d0c4cb0b34b17294934e0743
7Dagm21 <150895084+7Dagm21@users.noreply.github.com>
2025-07-17 11:10:06 +0300

    Add files via upload

---
+++ contact-form/src/App.tsx
@@ -0,0 +1,50 @@
+/*todolist
+import React, { useState } from "react";
+import ToDoList from "./types/todo";
+import TODOCrud from "./TODOCrud";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import "./index.css";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ToDoList[]>(TODOCrud.getTodos());
+
+  const handleAdd = (text: string) => {
+    const newTodo = TODOCrud.addTodos(text);
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
+  const handleUpdate = (todo: ToDoList) => {
+    TODOCrud.updateTodo(todo);
+    setTodos((prev) => prev.map((t) => (t.id === todo.id ? todo : t)));
+  };
+
+  const handleDelete = (id: number) => {
+    TODOCrud.deleteTodos(id);
+    setTodos((prev) => prev.filter((t) => t.id !== id));
+  };
+
+  return (
+    <div className="app-container">
+      <h1>Todo List</h1>
+      <TodoInput onAdd={handleAdd} />
+      <TodoList todos={todos} onUpdate={handleUpdate} onDelete={handleDelete} />
+    </div>
+  );
+};
+
+export default App;
+*/
+
+
+import ContactForm from "./components/ContactForm"; // or correct path
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
